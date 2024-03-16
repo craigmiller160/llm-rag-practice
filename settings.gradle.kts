e8998e0da97fea3f8c0c0ed4f456776ed26932e5
Craig Miller <craigmiller160@gmail.com>
2024-03-16 13:49:38 -0400

    Setting up a new module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ val projectName: String by settings
 
 rootProject.name = projectName
 
-include("libraries:common-spring", "low-level-v1")
+include("libraries:common-spring", "libraries:common-spring-openai", "low-level-v1")
