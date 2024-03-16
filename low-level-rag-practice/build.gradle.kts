6c581346ab8c35b80d98633debccbba693a80989
Craig Miller <craigmiller160@gmail.com>
2024-03-16 12:27:12 -0400

    Progress

--- low-level-rag-practice/build.gradle.kts
+++ low-level-rag-practice/build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val kotlinVersion = "2.0.0-Beta4"
+
     id("org.springframework.boot") version "3.2.3"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm")
-    id("io.craigmiller160.gradle.defaults") version "1.2.2"
-    kotlin("plugin.spring")
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
 }
 
 java {
