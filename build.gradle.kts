752814d2ba5fa391c3e12c2ad0a792386074d386
Craig Miller <craigmiller160@gmail.com>
2024-03-16 12:04:36 -0400

    Progress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 val projectGroup: String by project
 val projectVersion: String by project
 
-group = projectGroup
-version = projectVersion
+allprojects {
+    version = projectVersion
+    group = projectGroup
+}
