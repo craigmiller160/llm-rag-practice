74cd7b3188fe9757bc91ba6be560390c30f6e6d8
Craig Miller <craigmiller160@gmail.com>
2024-03-16 14:02:21 -0400

    Cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,9 @@ val projectName: String by settings
 
 rootProject.name = projectName
 
-include("libraries:common-spring", "libraries:common-spring-openai", "libraries:common-milvus", "low-level-v1")
+include(
+    "libraries:common-spring",
+    "libraries:common-spring-openai",
+    "libraries:common-milvus",
+    "low-level-v1"
+)
