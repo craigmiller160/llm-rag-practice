6883291a22e5dca1581ec9377d6aefbb12a4949b
Craig Miller <craigmiller160@gmail.com>
2024-03-16 16:01:33 -0400

    Updated libs

--- low-level-v2/build.gradle.kts
+++ low-level-v2/build.gradle.kts
@@ -18,6 +18,7 @@ java {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
