cea300b2933f1ac1f330234c5a752d1f5a8da900
Craig Miller <craigmiller160@gmail.com>
2024-03-19 19:49:22 -0400

    Progress

--- low-level-v2/src/main/kotlin/us/craigmiller160/llm/practice/lowlevel/v2/WeaviateExperiment.kt
+++ low-level-v2/src/main/kotlin/us/craigmiller160/llm/practice/lowlevel/v2/WeaviateExperiment.kt
@@ -1,6 +1,8 @@
 package us.craigmiller160.llm.practice.lowlevel.v2
 
 import io.weaviate.client.WeaviateClient
+import io.weaviate.client.v1.graphql.query.argument.NearVectorArgument
+import io.weaviate.client.v1.graphql.query.fields.Field
 import io.weaviate.client.v1.schema.model.DataType
 import io.weaviate.client.v1.schema.model.Property
 import io.weaviate.client.v1.schema.model.WeaviateClass
@@ -62,6 +64,8 @@ class WeaviateExperiment(
         .withProperties(mapOf(TEXT_FIELD to text1))
         .withVector(embedding1.toTypedArray())
         .run()
+        .toKotlinResult()
+        .getOrThrow()
 
     weaviateClient
         .data()
@@ -71,6 +75,29 @@ class WeaviateExperiment(
         .withProperties(mapOf(TEXT_FIELD to text2))
         .withVector(embedding2.toTypedArray())
         .run()
+        .toKotlinResult()
+        .getOrThrow()
     log.info("Documents and embeddings written to store")
+
+    log.info("Creating search query embedding")
+    val query = "What is your favorite sport?"
+    val searchEmbedding =
+        CreateEmbeddingRequest(model = EmbeddingModel.TEXT_EMBEDDING_3_SMALL, input = query)
+            .let { openaiClient.createEmbedding(it) }
+            .data
+            .flatMap { it.embedding }
+    log.info("Search query embedding created")
+
+    log.info("Performing search")
+    weaviateClient
+        .graphQL()
+        .get()
+        .withClassName(CLASS_NAME)
+        .withFields(Field.builder().name(TEXT_FIELD).build())
+        .withNearVector(NearVectorArgument.builder().vector(searchEmbedding.toTypedArray()).build())
+        .run()
+        .toKotlinResult()
+        .getOrThrow()
+    log.info("Search completed")
   }
 }
